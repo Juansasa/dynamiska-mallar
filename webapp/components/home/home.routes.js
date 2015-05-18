49abe7fc8e973c3800a2e2c87a26488eb21eb166
Quang vu <nickiewooster@gmail.com>
2015-05-18 15:22:55 +0200

    initial commit

---
+++ webapp/components/home/home.routes.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    angular.module('home')
+        .run(setUpRoutes);
+
+    /*@ngInject*/
+    function setUpRoutes(routeHelper, gettext) {
+        var stateName = gettext('Home');
+        var stateConfig = {
+            url: '/home',
+            templateUrl: 'components/home/home.html',
+            title: gettext('Home'),
+            controller: 'HomeController'
+        };
+
+        routeHelper.registerState(stateName, stateConfig);
+        routeHelper.setDefaultState(stateConfig.url);
+    }
+    setUpRoutes.$inject = ['routeHelper', 'gettext'];
+})();
---
+++ webapp/shared/translation/translation.pot.js
@@ -0,0 +1,5 @@
+angular.module('previa.order').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('sv', {"test":"Test translated"});
+/* jshint +W100 */
+}]);
